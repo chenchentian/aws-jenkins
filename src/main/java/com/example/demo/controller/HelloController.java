ce9e1013f9b32beba0e7a54264fb502dc8c2f7fd
chenhong.tian <chenhong.tian@thoughtworks.com>
2022-05-27 15:59:29 +0800

    github-actions

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@SuppressWarnings(value = "all")
 public class HelloController {
 
     @Autowired
